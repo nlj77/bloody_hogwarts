4de3d6a796e132768f7d54999e8c4b194da1ac6a
Nick Jones <nickjones9605@gmail.com>
2022-07-19 13:28:58 -0600

    Setup complete model tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,7 @@
 class Student < ApplicationRecord
-
+    validates_presence_of :name
+    validates_presence_of :house
+    validates_presence_of :age
+    has_many :student_courses
+    has_many :courses, through: :student_courses
 end
